ddd9549c550d9c707c1e48be31a27ace2c8842bc
seanblann <sblann@cnm.edu>
2022-02-08 14:29:37 -0700

    Basic build setup with verification of round trip.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/CodebreakerApplication.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/CodebreakerApplication.java
@@ -2,6 +2,9 @@ package edu.cnm.deepdive.codebreaker;
 
 import android.app.Application;
 import com.facebook.stetho.Stetho;
+import edu.cnm.deepdive.codebreaker.model.entity.Game;
+import edu.cnm.deepdive.codebreaker.service.CodebreakerServiceProxy;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class CodebreakerApplication extends Application {
 
@@ -9,6 +12,14 @@ public class CodebreakerApplication extends Application {
   public void onCreate() {
     super.onCreate();
     Stetho.initializeWithDefaults(this);
+    Game game = new Game();
+    game.setPool("ABC");
+    game.setLength(3);
+    CodebreakerServiceProxy
+        .getInstance()
+        .startGame(game)
+        .subscribeOn(Schedulers.io())
+        .subscribe();
     //Initialize database.
     //Initialize repositories that need an app-level context.
     //etc.
--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerServiceProxy.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerServiceProxy.java
@@ -2,12 +2,18 @@ package edu.cnm.deepdive.codebreaker.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.codebreaker.BuildConfig;
 import edu.cnm.deepdive.codebreaker.model.entity.Game;
 import edu.cnm.deepdive.codebreaker.model.entity.Guess;
+import io.reactivex.rxjava3.core.Single;
 import java.io.IOException;
 import java.util.Properties;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
 import retrofit2.Call;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
@@ -19,10 +25,10 @@ public interface CodebreakerServiceProxy {
   String ISO_8601_DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 
   @POST("games/")
-  Call<Game> startGame(@Body Game game);
+  Single<Game> startGame(@Body Game game);
 
   @POST("games/{gameId}/guesses")
-  Call<Guess> submitGuess(@Path("gameId") String gameID, @Body Guess guess);
+  Single<Guess> submitGuess(@Path("gameId") String gameID, @Body Guess guess);
 
   static CodebreakerServiceProxy getInstance(){
     return InstanceHolder.INSTANCE;
@@ -36,21 +42,22 @@ public interface CodebreakerServiceProxy {
 
 
     static {
-      try {
-        Gson gson = new GsonBuilder()
-            .excludeFieldsWithoutExposeAnnotation()
-            .setDateFormat(ISO_8601_DATETIME_FORMAT)
-            .create();
-        Properties props = new Properties();
-        props.load(InstanceHolder.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE));
-        Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(props.getProperty(BASE_URL_KEY))
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build();
-        INSTANCE = retrofit.create(CodebreakerServiceProxy.class);
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
+      Gson gson = new GsonBuilder()
+          .excludeFieldsWithoutExposeAnnotation()
+          .setDateFormat(ISO_8601_DATETIME_FORMAT)
+          .create();
+      HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(Level.BODY);
+      OkHttpClient client = new OkHttpClient.Builder()
+          .addInterceptor(interceptor)
+          .build();
+      Retrofit retrofit = new Retrofit.Builder()
+          .client(client)
+          .baseUrl(BuildConfig.BASE_URL)
+          .addConverterFactory(GsonConverterFactory.create(gson))
+          .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+          .build();
+      INSTANCE = retrofit.create(CodebreakerServiceProxy.class);
     }
 
 
