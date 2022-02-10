2983530d133beadc740f408399aa6dc07430eba4
seanblann <sblann@cnm.edu>
2022-02-10 11:05:02 -0700

    Started implementation for color based codes.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -20,11 +22,18 @@ import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentGameBinding;
 import edu.cnm.deepdive.codebreaker.model.entity.Guess;
 import edu.cnm.deepdive.codebreaker.viewmodel.GameViewModel;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameFragment extends Fragment {
 
   private GameViewModel gameViewModel;
   private FragmentGameBinding binding;
+  private Map<Character, Integer> colorValueMap;
+  private Map<Character, String> colorLabelMap;
+  private Character[] poolCharacters;
+  private Spinner[] spinners;
+  private int codeLength;
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,6 +52,7 @@ public class GameFragment extends Fragment {
           .trim();
       gameViewModel.submitGuess(text);
     });
+    setupMaps();
     return binding.getRoot();
   }
 
@@ -93,6 +103,36 @@ public class GameFragment extends Fragment {
   public void onDestroyView() {
     super.onDestroyView();
     binding = null;
+  }
+
+  private void setupMaps(){
+    String colors = getString(R.string.color_chars);
+    char[] colorChars = colors.toCharArray();
+    poolCharacters = colors
+        .chars()
+        .mapToObj((value) -> (char) value)
+        .toArray(Character[]::new);
+    Resources resources = getResources();
+    int[] colorValues = resources.getIntArray(R.array.color_values);
+    String[] colorLabels = resources.getStringArray(R.array.color_labels);
+    colorValueMap = buildValueMap(colorChars, colorValues);
+    colorLabelMap = buildLabelMap(colorChars, colorLabels);
+  }
+
+  private static Map<Character, Integer> buildValueMap(char[] chars, int[] values) {
+    Map<Character, Integer> valueMap = new HashMap<>();
+    for (int i = 0; i < chars.length; i++) {
+      valueMap.put(chars[i], values[i]);
+    }
+    return valueMap;
+  }
+
+  private static Map<Character, String> buildLabelMap(char[] chars, String[] labels) {
+    Map<Character, String> labelMap = new HashMap<>();
+    for (int i = 0; i < chars.length; i++) {
+      labelMap.put(chars[i], labels[i]);
+    }
+    return labelMap;
   }
 }
 
