ce0038a24fd48edd1ed1d23d1797440a23463130
seanblann <sblann@cnm.edu>
2022-02-21 15:08:55 -0700

    implemented settings screen display

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -1,6 +1,10 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -13,6 +17,8 @@ import edu.cnm.deepdive.codebreaker.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
   private ActivityMainBinding binding;
+  private AppBarConfiguration appBarConfiguration;
+  private NavController navController;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +30,39 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView navView = findViewById(R.id.nav_view);
     // Passing each menu ID as a set of Ids because each
     // menu should be considered as top level destinations.
-    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+    appBarConfiguration = new AppBarConfiguration.Builder(
         R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
         .build();
-    NavController navController = Navigation.findNavController(this,
+    navController = Navigation.findNavController(this,
         R.id.nav_host_fragment_activity_main);
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     NavigationUI.setupWithNavController(binding.navView, navController);
   }
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.main_options, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled;
+    int itemId = item.getItemId();
+    if (itemId == R.id.settings) {
+      Intent intent = new Intent();
+      startActivity(intent);
+      handled = true;
+    } else {
+      handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
+  }
+
+  @Override
+  public boolean onSupportNavigateUp() {
+    return NavigationUI.navigateUp(navController, appBarConfiguration)
+        || super.onSupportNavigateUp();
+  }
 }
---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/SettingsActivity.java
@@ -0,0 +1,24 @@
+package edu.cnm.deepdive.codebreaker.controller;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.preference.PreferenceFragmentCompat;
+import edu.cnm.deepdive.codebreaker.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_settings);
+  }
+
+  public static class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+      setPreferencesFromResource(R.xml.settings, rootKey);
+    }
+  }
+}
