ce85cd8840d3620adb37edc3ae346747a3daf47c
seanblann <sblann@cnm.edu>
2022-03-02 14:16:13 -0700

    Basic summary screen working.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/SummaryFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/SummaryFragment.java
@@ -10,11 +10,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentSummaryBinding;
 import edu.cnm.deepdive.codebreaker.viewmodel.GameViewModel;
 
 public class SummaryFragment extends Fragment implements OnSeekBarChangeListener {
 
+  private static final float MILLISECONDS_PER_SECOND = 1000.0f;
+  private static final int SECONDS_PER_MINUTE = 60;
   private GameViewModel viewModel;
   private FragmentSummaryBinding binding;
 
@@ -34,12 +37,27 @@ public class SummaryFragment extends Fragment implements OnSeekBarChangeListener
     viewModel
         .getSummary()
         .observe(getViewLifecycleOwner(),(summary) -> {
-          //TODO: Populate TextView objects in binding with summary data.
+          if (summary == null) {
+            binding.countGames.setText(String.valueOf(0));
+            binding.minGuesses.setText("");
+            binding.maxGuesses.setText("");
+            binding.avgGuesses.setText("");
+            binding.minTime.setText("");
+            binding.maxTime.setText("");
+            binding.avgTime.setText("");
+          } else {
+            binding.countGames.setText(String.valueOf(summary.getCountGames()));
+            binding.minGuesses.setText(String.valueOf(summary.getMinGuesses()));
+            binding.maxGuesses.setText(String.valueOf(summary.getMaxGuesses()));
+            binding.avgGuesses.setText(getString(R.string.avg_guess_format, summary.getAvgGuesses()));
+            binding.minTime.setText(getDuration(summary.getMinTime()));
+            binding.maxTime.setText(getDuration(summary.getMaxTime()));
+            binding.avgTime.setText(getDuration((int) Math.round(summary.getAvgTime())));
+          }
         });
-    viewModel.getLength()
-        .observe(getViewLifecycleOwner(), (length) -> {
-          //TODO: Update SeekBar in binding if necessary.
-        });
+    viewModel
+        .getLength()
+        .observe(getViewLifecycleOwner(), (length) -> binding.length.setProgress(length));
   }
 
   @Override
@@ -58,11 +76,18 @@ public class SummaryFragment extends Fragment implements OnSeekBarChangeListener
 
   @Override
   public void onStartTrackingTouch(SeekBar seekBar) {
-
+    // do nothing
   }
 
   @Override
   public void onStopTrackingTouch(SeekBar seekBar) {
+    // do nothing
+  }
 
+  private String getDuration(int milliseconds) {
+    int seconds = Math.round(milliseconds / MILLISECONDS_PER_SECOND);
+    int minutes = seconds / SECONDS_PER_MINUTE;
+    seconds %= SECONDS_PER_MINUTE;
+    return getString(R.string.mmss_format, minutes, seconds);
   }
 }
